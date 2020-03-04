ecc8d449f04faa3c5d0aa33ed09c9e5d7125f343
chaya-tan <chayatan.ang@gmail.com>
2020-03-04 20:25:42 +0700

    set background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="layout">
     <nuxt />
   </div>
 </template>
+
+<style scoped>
+.layout {
+  background-image: url('~assets/images/background/geometry2.png');
+}
+</style>
